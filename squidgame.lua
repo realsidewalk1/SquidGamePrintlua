d430bb09cf95fb233daeefefbbbd41ecd775eaf7
realsidewalk1 <96834657+realsidewalk1@users.noreply.github.com>
2022-03-15 21:07:38 +0100

    Create squidgame.lua

---
+++ squidgame.lua
@@ -0,0 +1,55 @@
+v = "v1.0"
+owner = "sidewalk's Squid Game prints game"
+
+function wait(seconds)
+    local start = os.time()
+    repeat until os.time() > start + seconds
+end
+
+print(owner)
+print(v) if print then
+    -- Setting up prints. Not needed just for fun.
+    wait(1)
+    print("Setting up game")
+    wait(1)
+    print("Setting up prints")
+    wait(1)
+    print("Ready to start")
+    wait(0.5)
+  -- Lobby prints
+    print("3!")
+    wait(0.5)
+    print("2!")
+    wait(0.5)
+    print("1!")
+    wait(0.5)
+    print("Game started!")
+    -- Squid Game Prints
+    wait(1.5)
+    print("Green light.")
+    wait(3.0)
+    print("Red light.")
+    wait(2.0)
+    print("Green light.")
+    wait(4.0)
+    print("Red light.")
+    wait(1.5)
+    print("Green light.")
+    wait(3.0)
+    print("Red light.")
+    wait(2.0)
+    print("Green light.")
+    wait(4.0)
+    print("Red light.")
+    wait(1.5)
+    print("Green light.")
+    wait(3.0)
+    print("Red light.")
+    wait(2.0)
+    print("Green light.")
+    wait(4.0)
+    print("Red light.")
+    wait(1)
+    -- Done
+    print("You finished the game. Good Job")
+end
